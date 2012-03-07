3c8c321972537534a60a852783fa2e507c42647f
adius <adriansieber@web.de>
2012-03-07 21:42:14 +0100

    shortend syntax

--- src/dominate.js
+++ src/dominate.js
@@ -9,15 +9,15 @@ function DOMinate(a, b, c) {
         return b
     }
 
-    if (a[0].big) a[0] = d(a[0]);
+    if (a[0].big)
+        a[0] = d(a[0]);
 
     for (c = 1; c < a.length; c++) {
         if (a[c].big)
-            a[0].innerHTML = a[c];
+            a[0].innerHTML = a[c]
 
         else if (a[c].pop)
-            a[c][0] = d(a[c][0]),
-            a[0].appendChild(a[c][0]),
+            a[0].appendChild(a[c][0] = d(a[c][0])),
             DOMinate(a[c])
 
         else
