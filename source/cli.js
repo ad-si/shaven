13beca1b58348272cef4e362c8eb84e895913ed4
Adrian Sieber <mail@adriansieber.com>
2015-11-21 15:07:30 +0100

    Add command line interface

---
+++ source/cli.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+'use strict'
+
+const stream = require('stream')
+const path = require('path')
+const html2shaven = require('./index.js')
+
+const usage = `Usage: ${path.basename(process.argv[1])} < file.html > file.json`
+let internalBuffer = ''
+
+if (process.stdin.isTTY) {
+	console.error(usage)
+	process.exit(1)
+}
+else {
+	process.stdin
+		.pipe(new stream.Transform({
+			write (chunk, encoding, done) {
+				internalBuffer += chunk.toString()
+				done()
+			},
+			flush (done) {
+				this.push(JSON.stringify(
+					html2shaven(internalBuffer), null, 4
+				) + '\n')
+				done()
+			}
+		}))
+		.pipe(process.stdout)
+}
