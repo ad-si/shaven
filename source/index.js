d6e236cf7254fdfd35f541d7b35c98a756c1bac0
Adrian Sieber <mail@adriansieber.com>
2015-11-21 15:02:58 +0100

    Initial commit

---
+++ source/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const parse5 = require('parse5')
+const minifier = require('html-minifier')
+
+let parser = new parse5.Parser()
+
+
+function walkTree (node) {
+	if (node.nodeName === '#document') {
+		return walkTree(node.childNodes[1])
+	}
+
+	if (Array.isArray(node))
+		return node.map(walkTree)
+
+	if (node.childNodes)
+		return [node.nodeName, ...walkTree(node.childNodes)]
+
+	if (node.nodeName === '#documentType')
+		return ''
+
+	if (node.nodeName !== '#comment')
+		return node.value
+}
+
+module.exports = function (htmlString) {
+
+	let minifiedHtmlString = minifier.minify(
+		htmlString,
+		{
+			collapseWhitespace: true
+		}
+	)
+	let document = parser.parse(minifiedHtmlString)
+
+	return walkTree(document)
+}
