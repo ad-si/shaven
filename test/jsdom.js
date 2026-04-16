57195c868ffa7e4be83c317a7f0d39375d103f6e
Adrian Sieber <mail@adriansieber.com>
2026-04-16 10:16:26 +0000

    Simplify JSDOM global setup in test harness

--- test/jsdom.js
+++ test/jsdom.js
@@ -2,11 +2,10 @@ import jsdom from 'jsdom'
 import shaven from '../source/library/browser.js'
 
 const {JSDOM} = jsdom
+const {window} = new JSDOM()
 
 global.shaven = shaven
-global.document = new JSDOM().window.document
-global.window = document.defaultView
-global.navigator = window.navigator
+global.document = window.document
 global.Element = window.Element
 
 import './core.js'
